4cf4ed37ec836154eb9ce3427b38882cd4774f56
Chandana R <134958930+Chandana1711@users.noreply.github.com>
2024-10-11 19:34:48 +0530

    Time: 34 ms (85.43%), Space: 16.6 MB (26.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,7 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 0:
-            return None
+            return None                   #Newton's Algorithm
         guess= x/2
         tolerance = 0.000001
         
